ede8dfb7dea29618946a5331d5e84cc9107f75ee
enzogurgel <egb2@cin.ufpe.br>
2025-08-02 10:00:27 -0300

    adiciona comando utilitario para enviar e re-buildar projeto

--- funcs.zsh
+++ funcs.zsh
@@ -2,6 +2,10 @@ cp2pi() {
   scp $1 robot@192.168.1.100:$2
 }
 
+cp2pir() {
+	scp -r $1 robot@192.168.1.100:$2 && ssh robot@192.168.1.100 "cd /home/robot/maze-runner/ros;source ./install/setup.bash;source ./install/local_setup.bash;colcon build"
+}
+
 genssh() {
 	ssh-keygen -t rsa
 	cat ~/.ssh/id_rsa.pub | ssh robot@192.168.1.100 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
